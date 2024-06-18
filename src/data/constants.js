a6c66ee050e8566e847d194f2bc9bb1176969974
Vivek Kumar <148897808+codemonster57@users.noreply.github.com>
2024-06-18 12:33:40 +0530

    Update constants.js resume updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/codemonster57",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1RXoKh0xwtcw3BiEDU3vFnqh2TStT-hYS/view",
   linkedin: "https://www.linkedin.com/in/vivek-kumar-06454324b/",
   twitter: "https://x.com/vivekkumr0710",
   insta: "https://www.instagram.com/kumrvivek.azad/",
